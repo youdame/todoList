f8f971de09a13236f6eca35762c1c59df7b23f9c
youdame <112458620+youdame@users.noreply.github.com>
2024-01-09 11:29:48 +0900

    feat : todo 프로젝트

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <button>all</button>
+      </li>
+      <li>
+        <button>active</button>
+      </li>
+      <li>
+        <button>completed</button>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
